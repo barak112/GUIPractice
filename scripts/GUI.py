38a14184b596303087e0dc9cb87422eafa81df53
RonHabaron <barakbm9@gmail.com>
2023-02-02 16:04:21 +0200

    subwidgets sizes ready, about to try some improvements

--- scripts/GUI.py
+++ scripts/GUI.py
@@ -58,12 +58,14 @@ def rgb2hex(r,g,b):
     return "#{:02x}{:02x}{:02x}".format(r,g,b)
 
 def change_UI_color():
-    # color = '#65A8E1'
     color = (rgb2hex(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
     root.configure(bg=color)
     sub_widgetsframe.configure(bg=color,  borderwidth=0)
 
-
+def default_UI_color():
+    color = "#ffffff"
+    root.configure(bg=color)
+    sub_widgetsframe.configure(bg=color,  borderwidth=0)
 
 sub_widgetsframe = tk.LabelFrame(root, bg="White")
 columnconfigure(sub_widgetsframe, 3)
@@ -77,7 +79,7 @@ columnconfigure(widget1frame, 3)
 right_arrow_image = tk.PhotoImage(file="assets/right.png")
 left_arrow_image = tk.PhotoImage(file="assets/left.png")
 
-widget1_title = tk.Label(widget1frame, text="Value", font=("Ariel", 43), bg="LightGray")
+widget1_title = tk.Label(widget1frame, text="Value", font=("Ariel", 38), bg="LightGray")
 widget1_downbutton = tk.Button(widget1frame, image=left_arrow_image, bg="LightGray", command=minus_number)
 widget1_upbutton = tk.Button(widget1frame, image=right_arrow_image, bg="LightGray", command=plus_number)
 widget1_number_label = tk.Label(widget1frame, textvariable=widget1_number_text, font=("Arial", 25), bg="LightGray")
@@ -87,41 +89,37 @@ widget1_downbutton.grid(row=1, column=0, padx=25)
 widget1_upbutton.grid(row=1, column=2, padx=25)
 widget1_number_label.grid(row=1, column=1)
 
-widget1frame.grid(row=0, column=0, ipady=148.5, sticky="wn")
+widget1frame.grid(row=0, column=0, sticky="wn", ipady=112)
+#size = 346^2
 
 
 widget2frame = tk.LabelFrame(sub_widgetsframe, bg="LightGray")
 rowconfigure(widget2frame, 2)
 
 widget2_title = tk.Label(widget2frame, bg="LightGray", text="Did you know?", font=("Arial", 40))
-widget2_content = tk.Label(widget2frame, bg="LightGray", text="I'm more of a backend developer than a front end developer (better at logical coding than UI designing)", font=("Arial", 25), wraplength=250)
+widget2_content = tk.Label(widget2frame, bg="LightGray", text="I'm more of a backend developer than a front end developer (better at logical coding than UI designing)", font=("Arial", 25), wraplength=275)
 
 
 widget2_title.grid(row=0, column=0)
 widget2_content.grid(row=1,column=0)
-widget2frame.grid(row=0, column=1, sticky="n", ipady=25)
+widget2frame.grid(row=0, column=1, sticky="n", ipady=4)
+# size = 346px^2
 
 
 widget3frame = tk.LabelFrame(sub_widgetsframe, bg="LightGray")
 rowconfigure(widget3frame, 3)
+columnconfigure(widget3frame, 1)
 
 color_entry = tk.StringVar()
 widget3_title = tk.Label(widget3frame, bg="LightGray", text="UI Color", font=("Arial", 40))
-widget3_change_color_button = tk.Button(widget3frame, bg="#0099ff", text="Change Color", font=("Arial", 25), command=change_UI_color)
-widget3_color_entryfield = tk.Entry(widget3frame, font=("Arial", 25))
+widget3_change_color_button = tk.Button(widget3frame, bg="#0099ff", text="Change Color", font=("Arial", 20), command=change_UI_color)
+widget3_default_color_button = tk.Button(widget3frame,  bg="#0099ff", text="Default Color", font=("Arial", 20), command=default_UI_color)
 
 widget3_title.grid(row=0, column=0)
-widget3_change_color_button.grid(row=1,column=0, ipady=25, ipadx=50)
-widget3_color_entryfield.grid(row=2, column=0, pady=25, ipady=25)
-widget3frame.grid(row=0, column=2, sticky="ne", ipady=50)
-
-
-def enter(event):
-    if event.keysym=='enter':
-        pass
-
-root.bind("<Key>", enter)
-
+widget3_change_color_button.grid(row=1,column=0)
+widget3_default_color_button.grid(row=2, column=0, pady=25)
+widget3frame.grid(row=0, column=2, sticky="ne", ipadx=73, ipady=59)
+#size = 346px^2
 
 sub_widgetsframe.pack(fill="x", anchor="center", padx=300)
 
